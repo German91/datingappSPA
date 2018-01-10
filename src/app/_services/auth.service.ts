b23b91256f92694b9ae301275f36b6a02b117faf
German <german.rosario@financial-cloud.com>
2018-01-10 12:58:20 +0000

    Added alertify for notifications, jwthelper to handle token authentication, ngx-bootstrap for dropdown and united bootstrap theme.

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -4,11 +4,14 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import { Observable } from 'rxjs/Observable';
+import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
     baseUrl = 'http://localhost:5000/api/auth/';
     userToken: any;
+    decodedToken: any;
+    jwtHelper: JwtHelper = new JwtHelper();
 
     constructor(private http: Http) { }
 
@@ -18,12 +21,17 @@ export class AuthService {
             if (user) {
                 localStorage.setItem('token', user.tokenString);
                 this.userToken = user.tokenString;
+                this.decodedToken = this.jwtHelper.decodeToken(this.userToken);
             }
         }).catch(this.handleError);
     }
 
     register(model: any) {
         return this.http.post(this.baseUrl + 'register', model, this.requestOptions()).catch(this.handleError);
+    }
+
+    loggedIn() {
+        return tokenNotExpired();
     }
 
     private requestOptions() {
