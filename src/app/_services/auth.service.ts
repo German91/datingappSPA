75418d2cc854cdb09fb29fda73dc2ca9bee0514f
German <german.rosario@financial-cloud.com>
2018-01-10 11:33:11 +0000

    Catching errors from API

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthService {
@@ -16,15 +19,34 @@ export class AuthService {
                 localStorage.setItem('token', user.tokenString);
                 this.userToken = user.tokenString;
             }
-        });
+        }).catch(this.handleError);
     }
 
     register(model: any) {
-        return this.http.post(this.baseUrl + 'register', model, this.requestOptions());
+        return this.http.post(this.baseUrl + 'register', model, this.requestOptions()).catch(this.handleError);
     }
 
     private requestOptions() {
         const headers = new Headers({ 'Content-type': 'application/json' });
         return new RequestOptions({ headers });
+    }
+
+    private handleError(error: any) {
+        const applicationError = error.headers.get('Application-Error');
+        if (applicationError) {
+            return Observable.throw(applicationError);
+        }
+        const serverError = error.json();
+        let modelStateError = '';
+        if (serverError) {
+            for (const key in serverError) {
+                if (serverError[key]) {
+                    modelStateError += serverError[key] + '\n';
+                }
+            }
+        }
+        return Observable.throw(
+            modelStateError || 'Server error'
+        );
     }
 }
